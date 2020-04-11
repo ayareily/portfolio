797bd408b307f100babc1957d9fc58a0c75bec14
ojou <musiclear05@gmail.com>
2020-04-11 20:53:51 +0900

    article edit

---
+++ resources/views/articles/form.blade.php
@@ -0,0 +1,14 @@
+@csrf
+<div class="md-form">
+  <label>タイトル</label>
+  <input type="text" name="title" class="form-control" required value="{{ old('title') }}">
+</div>
+<div class="form-group">
+  <label></label>
+  <textarea name="body" required class="form-control" rows="16" placeholder="本文">{{ old('body') }}</textarea>
+
+<div class="md-form">
+<label>投稿日</label>
+<input type="date" name="published_at" required class="form-control">
+</div>
+</div>
