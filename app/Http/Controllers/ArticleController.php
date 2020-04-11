eef514025f664f1a856778d9cdfbfc745bb84fc3
ojou <musiclear05@gmail.com>
2020-04-11 18:50:31 +0900

    diary

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -6,6 +6,10 @@ use Illuminate\Http\Request;
 
 class ArticleController extends Controller
 {
+    public function diary() {
+        return view('diary');
+    }
+
     public function create() {
         return view('articles.create');
     } //
