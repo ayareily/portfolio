13f048734b3519b9a6c9d4155bea85479c43a375
ojou <musiclear05@gmail.com>
2020-04-15 13:15:19 +0900

    vue edit

---
+++ resources/js/components/ArticleTagsInput.vue
@@ -0,0 +1,59 @@
+<template>
+  <div>
+    <vue-tags-input
+      v-model="tag"
+      :tags="tags"
+      :autocomplete-items="filteredItems"
+      @tags-changed="newTags => tags = newTags"
+    />
+  </div>
+</template>
+
+<script>
+import VueTagsInput from '@johmun/vue-tags-input';
+
+export default {
+  components: {
+    VueTagsInput,
+  },
+  data() {
+    return {
+      tag: '',
+      tags: [],
+      autocompleteItems: [{
+        text: 'Laravel',
+      }, {
+        text: 'PHP',
+      }, {
+        text: 'Node.js',
+      }, {
+        text: 'N予備校',
+      }, {
+        text: 'はなまる',
+      }],
+    };
+  },
+  computed: {
+    filteredItems() {
+      return this.autocompleteItems.filter(i => {
+        return i.text.toLowerCase().indexOf(this.tag.toLowerCase()) !== -1;
+      });
+    },
+  },
+};
+</script>
+<style lang="css" scoped>
+  .vue-tags-input {
+    max-width: inherit;
+  }
+</style>
+<style lang="css">
+  .vue-tags-input .ti-tag {
+    background: transparent;
+    border: 1px solid #747373;
+    color: #747373;
+    margin-right: 4px;
+    border-radius: 0px;
+    font-size: 13px;
+  }
+</style>
