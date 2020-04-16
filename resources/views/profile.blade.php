a7cf3a8cae82c2ed3149b9283dba3f7aacf5bb2d
ojou <musiclear05@gmail.com>
2020-04-16 18:09:31 +0900

    profile edit

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -4,6 +4,71 @@
 
 @section('content')
   @include('nav')
-<img src="">
-<h2>Sogetsu Sato</h2>
+<h2 class="text-center mt-5">Sogetsu Sato</h2>
+
+<h3 class="text-center mt-3">Skills</h3>
+  <div class="container">
+  <div class="row m-0 p-0">
+    <div class="card col-sm-2 m-2">
+      <div class="card-body text-center">
+    <h5 class="card-title">XHTML</h5>
+    <i class="fas fa-code fa-3x pb-3"></i>
+    <h6 class="card-subtitle mb-2 text-muted">2005</h6>
+  </div>
+  </div>
+<div class="card col-sm-2 m-2">
+  <div class="card-body text-center">
+    <h5 class="card-title">Twitter Bot(PHP)</h5>
+    <i class="fab fa-twitter fa-3x pb-3"></i>
+    <h6 class="card-subtitle mb-2 text-muted">2009</h6>
+  </div>
+</div>
+
+<div class="card col-sm-2 m-2">
+  <div class="card-body text-center">
+    <h5 class="card-title">CSS</h5>
+    <i class="far fa-file-code fa-3x pb-3"></i>
+    <h6 class="card-subtitle mb-2 text-muted">2013</h6>
+  </div>
+</div>
+
+<div class="card col-sm-2 m-2">
+  <div class="card-body text-center">
+    <h5 class="card-title">WordPress</h5>
+    <i class="fab fa-wordpress-simple fa-3x pb-3"></i>
+    <h6 class="card-subtitle mb-2 text-muted">2017</h6>
+  </div>
+</div>
+
+<div class="card col-sm-2 m-2">
+  <div class="card-body text-center">
+    <h5 class="card-title">JavaScript(GAS)</h5>
+    <i class="fab fa-js-square fa-3x pb-3"></i>
+    <h6 class="card-subtitle mb-2 text-muted">2019</h6>
+  </div>
+</div>
+
+<div class="card col-sm-2 m-2">
+  <div class="card-body text-center">
+    <h5 class="card-title">PHP</h5>
+    <i class="fab fa-php fa-3x pb-3"></i>
+    <h6 class="card-subtitle mb-2 text-muted">2020</h6>
+  </div>
+</div>
+
+<div class="card col-sm-2 m-2">
+  <div class="card-body text-center">
+    <h5 class="card-title">Laravel</h5>
+    <i class="fab fa-laravel fa-3x pb-3"></i>
+    <h6 class="card-subtitle mb-2 text-muted">2020</h6>
+  </div>
+</div>
+
+<div class="card col-sm-2 m-2">
+  <div class="card-body text-center">
+    <h5 class="card-title">Bootstrap</h5>
+    <i class="fab fa-bootstrap fa-3x pb-3"></i>
+    <h6 class="card-subtitle mb-2 text-muted">2020</h6>
+  </div>
+</div>
 @endsection
