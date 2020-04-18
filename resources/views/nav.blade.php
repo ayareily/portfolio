6898fccf17cdee10aa1eb7fbce4e19e98cb86891
ojou <musiclear05@gmail.com>
2020-04-18 16:26:21 +0900

    navbar edit

--- resources/views/nav.blade.php
+++ resources/views/nav.blade.php
@@ -1,28 +1,27 @@
-<nav class="navbar navbar-expand-lg navbar-dark purple-gradient">
+<nav class="navbar navbar-expand-sm navbar-dark purple-gradient">
 <a class="navbar-brand" href="{{ route('index') }}"><i class="fab fa-fort-awesome mr-1 pr-0"></i>Clerk E's Portrolio</a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarToggler" aria-controls="navbarToggler" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
-
     <div class="collapse navbar-collapse" id="navbarToggler">
-        
-    <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
-            <li class="nav-item mr-3 ml-3">
-      <a class="nav-link" href="{{ route('index') }}">HOME</a>
-    </li>
-  <li class="nav-item mr-3 ml-3">
-      <a class="nav-link" href="{{ route('profile') }}">PROFILE</a>
-    </li>
-    <li class="nav-item mr-3 ml-3">
-      <a class="nav-link" href="{{ route('diary') }}">DIARY</a>
-    </li>
-
-    <li class="nav-item">
-      <a class="nav-link" href="https://twitter.com/ayareily"><i class="fab fa-twitter fa-lg"></i></a>
-    </li>
-    <li class="nav-item">
-      <a class="nav-link" href="https://github.com/ayareily/"><i class="fab fa-github-alt fa-lg"></i></a>
-    </li>
+    <ul class="navbar-nav justify-content-center mt-2 ml-5 mt-lg-0">
+      <li class="nav-item mr-3 ml-3">
+        <a class="nav-link" href="{{ route('index') }}">HOME</a>
+      </li>
+       <li class="nav-item mr-3 ml-3">
+        <a class="nav-link" href="{{ route('profile') }}">PROFILE</a>
+      </li>
+      <li class="nav-item mr-3 ml-3">
+        <a class="nav-link" href="{{ route('diary') }}">DIARY</a>
+      </li>
+    </ul>
+    <ul class="navbar-nav ml-auto">
+      <li class="nav-item">
+        <a class="nav-link" href="https://twitter.com/ayareily"><i class="fab fa-twitter fa-lg"></i></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="https://github.com/ayareily/"><i class="fab fa-github-alt fa-lg"></i></a>
+      </li>
 
     @auth
     <li class="nav-item">
@@ -49,6 +48,6 @@
     <!-- Dropdown -->
     @endauth
 
-  </ul>
+    </ul>
 
 </nav>
