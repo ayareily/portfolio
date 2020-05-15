cfe8608ea8b01e9de73a28521c9a9768be2e3936
ojou <musiclear05@gmail.com>
2020-05-16 01:00:39 +0900

    profile edit

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -10,80 +10,56 @@
       <h3 class="text-center mt-3">触ってきた技術たち</h3>
         <div class="row justify-content-center" id="profileskills">
 
-          <div class="card col-md-2 m-2">
-            <a href="{{ route('tags.show', ['name' => 'HTML']) }}">
-              <div class="card-body text-center">
-                <h5 class="card-title">XHTML</h5>
-                <i class="si fas fa-code fa-3x pb-3"></i>
-                <h6 class="card-subtitle mb-2">2005</h6>
-              </div>
-            </a>
-          </div>
+          <div class="card col-md-10 m-2">
+            <ul class="list-group list-group-flush">
+            <li class="list-group-item">
+            <h5 class="list-group-item-heading">2005</h5>
+            <a class="d-flex align-items-end" href="{{ route('tags.show', ['name' => 'HTML']) }}">
+            <i class="si fas fa-code fa-2x mr-2"></i>XHTML</a>
+            </li>
 
-          <div class="card col-md-2 m-2">
-            <div class="card-body text-center">
-              <h5 class="card-title">Twitter Bot(PHP)</h5>
-              <i class="si fab fa-twitter fa-3x pb-3"></i>
-              <h6 class="card-subtitle mb-2">2009</h6>
-            </div>
-          </div>
+            <li class="list-group-item">
+            <h5 class="list-group-item-heading">2009</h5>
+            <p class="d-flex align-items-end">
+            <i class="si fab fa-twitter fa-2x mr-2"></i>Twitter bot(PHP)</p>
+            </li>
 
-          <div class="card col-md-2 m-2">
-            <a href="{{ route('tags.show', ['name' => 'CSS']) }}">
-              <div class="card-body text-center">
-                <h5 class="card-title">CSS</h5>
-                <i class="si far fa-file-code fa-3x pb-3"></i>
-                <h6 class="card-subtitle mb-2">2013</h6>
-              </div>
-              </a>
-          </div>
+            <li class="list-group-item">
+            <h5 class="list-group-item-heading">2013</h5>
+            <a class="d-flex align-items-end" href="{{ route('tags.show', ['name' => 'CSS']) }}">
+            <i class="si far fa-file-code fa-2x mr-2"></i>CSS</a>
+            </li>
 
-          <div class="card col-md-2 m-2">
-            <div class="card-body text-center">
-              <h5 class="card-title">WordPress</h5>
-              <i class="si fab fa-wordpress-simple fa-3x pb-3"></i>
-              <h6 class="card-subtitle mb-2">2017</h6>
-            </div>
-          </div>
+            <li class="list-group-item">
+            <h5 class="list-group-item-heading">2017</h5>
+            <p class="d-flex align-items-end">
+            <i class="si fab fa-wordpress-simple fa-2x mr-2"></i>WordPress</p>
+            </li>
 
-          <div class="card col-md-2 m-2">
-            <a href="{{ route('tags.show', ['name' => 'GAS']) }}">
-              <div class="card-body text-center">
-                <h5 class="card-title">GAS</h5>
-                <i class="si fab fa-google fa-3x pb-3"></i>
-                <h6 class="card-subtitle mb-2">2019</h6>
-              </div>
-            </a>
-          </div>
+            <li class="list-group-item">
+            <h5 class="list-group-item-heading">2019.8</h5>
+            <a class="d-flex align-items-end" href="{{ route('tags.show', ['name' => 'PC修理']) }}">
+            <i class="si fas fa-wrench fa-2x mr-2"></i>PC Repair</p>
+            </li>
 
-          <div class="card col-md-2 m-2">
-            <a href="{{ route('tags.show', ['name' => 'PC修理']) }}">
-              <div class="card-body text-center">
-                <h5 class="card-title">PC Repair</h5>
-                <i class="si fas fa-wrench fa-3x pb-3"></i>
-                <h6 class="card-subtitle mb-2">2019</h6>
-              </div>
-            </a>
-          </div>
+            <li class="list-group-item">
+            <h5 class="list-group-item-heading">2019.10</h5>
+            <a class="d-flex align-items-end" href="{{ route('tags.show', ['name' => 'GAS']) }}">
+            <i class="si fab fa-google fa-2x mr-2"></i>GAS</p>
+            </li>
+            
+            <li class="list-group-item">
+            <h5 class="list-group-item-heading">2020.2</h5>
+            <a class="d-flex align-items-end" href="{{ route('tags.show', ['name' => 'JavaScript']) }}">
+            <i class="si fab fa-js-square fa-2x mr-2"></i>JavaScript</p>
+            </li>
 
-          <div class="card col-md-2 m-2">
-            <a href="{{ route('tags.show', ['name' => 'JavaScript']) }}">
-              <div class="card-body text-center">
-                <h5 class="card-title">JavaScript</h5>
-                <i class="si fab fa-js-square fa-3x pb-3"></i>
-                <h6 class="card-subtitle mb-2">2020</h6>
-              </div>
-            </a>
-          </div>
+            <li class="list-group-item">
+            <h5 class="list-group-item-heading">2020.3</h5>
+            <a class="d-flex align-items-end" href="{{ route('tags.show', ['name' => 'PHP']) }}">
+            <i class="si fab fa-php fa-2x mr-2"></i>PHP</p>
+            </ul>
 
-          <div class="card col-md-2 m-2">
-            <a href="{{ route('tags.show', ['name' => 'PHP']) }}">
-              <div class="card-body text-center">
-                <h5 class="card-title">PHP</h5>
-                <i class="si fab fa-php fa-3x pb-3"></i>
-                <h6 class="card-subtitle mb-2">2020</h6>
-              </div>
-            </a>
           </div>
 
           <div class="card col-md-2 m-2">
