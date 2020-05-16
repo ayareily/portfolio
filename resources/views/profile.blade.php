8df4edba23e2ec9602c9607d5750128dce064a09
ojou <musiclear05@gmail.com>
2020-05-16 20:35:15 +0900

    profile edit OK

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -7,129 +7,87 @@
   <div class="container-fluid justify-content-center mb-5" style="background-color: #FDEDFB; margin-top: 56px;">
     <img src="{{ asset('/img/profile.jpg') }}" class="rounded-circle mx-auto d-block p-3" style="height:50vh">
       <h2 class="text-center">Satomi Sato</h2>
-      <h3 class="text-center mt-3">触ってきた技術たち</h3>
         <div class="row justify-content-center" id="profileskills">
 
           <div class="card col-md-10 m-2">
-            <ul class="list-group list-group-flush">
-            <li class="list-group-item">
-            <h5 class="list-group-item-heading">2005</h5>
-            <a class="d-flex align-items-end" href="{{ route('tags.show', ['name' => 'HTML']) }}">
-            <i class="si fas fa-code fa-2x mr-2"></i>XHTML</a>
-            </li>
+            <h3 class="card-title text-center mt-3">触ってきた技術たち</h3>
+              <ul class="list-group list-group-flush">
+                <li class="list-group-item">
+                  <h5 class="list-group-item-heading">2005</h5>
+                    <a class="d-flex align-items-end" href="{{ route('tags.show', ['name' => 'HTML']) }}">
+                    <i class="si fas fa-code fa-2x mr-2"></i>XHTML</a>
+                </li>
 
-            <li class="list-group-item">
-            <h5 class="list-group-item-heading">2009</h5>
-            <p class="d-flex align-items-end">
-            <i class="si fab fa-twitter fa-2x mr-2"></i>Twitter bot(PHP)</p>
-            </li>
+                <li class="list-group-item">
+                  <h5 class="list-group-item-heading">2009</h5>
+                    <p class="d-flex align-items-end mb-2" style="color: #C71585;">
+                    <i class="si fab fa-twitter fa-2x mr-2"></i>Twitter bot(PHP)</p>
+                </li>
 
-            <li class="list-group-item">
-            <h5 class="list-group-item-heading">2013</h5>
-            <a class="d-flex align-items-end" href="{{ route('tags.show', ['name' => 'CSS']) }}">
-            <i class="si far fa-file-code fa-2x mr-2"></i>CSS</a>
-            </li>
+                <li class="list-group-item">
+                  <h5 class="list-group-item-heading">2013</h5>
+                    <a class="d-flex align-items-end mb-2" href="{{ route('tags.show', ['name' => 'CSS']) }}">
+                    <i class="si fas fa-code fa-2x mr-2"></i>CSS</a>
+                </li>
 
-            <li class="list-group-item">
-            <h5 class="list-group-item-heading">2017</h5>
-            <p class="d-flex align-items-end">
-            <i class="si fab fa-wordpress-simple fa-2x mr-2"></i>WordPress</p>
-            </li>
+                <li class="list-group-item">
+                  <h5 class="list-group-item-heading">2017</h5>
+                    <p class="d-flex align-items-end mb-2" style="color: #C71585;">
+                    <i class="si fab fa-wordpress-simple fa-2x mr-2"></i>WordPress</p>
+                </li>
 
-            <li class="list-group-item">
-            <h5 class="list-group-item-heading">2019.8</h5>
-            <a class="d-flex align-items-end" href="{{ route('tags.show', ['name' => 'PC修理']) }}">
-            <i class="si fas fa-wrench fa-2x mr-2"></i>PC Repair</p>
-            </li>
+                <li class="list-group-item">
+                  <h5 class="list-group-item-heading">2019.8</h5>
+                    <a class="d-flex align-items-end mb-2" href="{{ route('tags.show', ['name' => 'PC修理']) }}">
+                    <i class="si fas fa-wrench fa-2x mr-2"></i>PC Repair</a>
+                </li>
 
-            <li class="list-group-item">
-            <h5 class="list-group-item-heading">2019.10</h5>
-            <a class="d-flex align-items-end" href="{{ route('tags.show', ['name' => 'GAS']) }}">
-            <i class="si fab fa-google fa-2x mr-2"></i>GAS</p>
-            </li>
-            
-            <li class="list-group-item">
-            <h5 class="list-group-item-heading">2020.2</h5>
-            <a class="d-flex align-items-end" href="{{ route('tags.show', ['name' => 'JavaScript']) }}">
-            <i class="si fab fa-js-square fa-2x mr-2"></i>JavaScript</p>
-            </li>
+                <li class="list-group-item">
+                  <h5 class="list-group-item-heading">2019.10</h5>
+                    <a class="d-flex align-items-end mb-2" href="{{ route('tags.show', ['name' => 'GAS']) }}">
+                    <i class="si fab fa-google fa-2x mr-2"></i>GAS</a>
+                </li>
+                
+                <li class="list-group-item">
+                  <h5 class="list-group-item-heading">2020.2</h5>
+                    <a class="d-flex align-items-end mb-2" href="{{ route('tags.show', ['name' => 'JavaScript']) }}">
+                    <i class="si fab fa-js-square fa-2x mr-2"></i>JavaScript</a>
+                </li>
 
-            <li class="list-group-item">
-            <h5 class="list-group-item-heading">2020.3</h5>
-            <a class="d-flex align-items-end" href="{{ route('tags.show', ['name' => 'PHP']) }}">
-            <i class="si fab fa-php fa-2x mr-2"></i>PHP</p>
-            </ul>
+                <li class="list-group-item">
+                  <h5 class="list-group-item-heading">2020.3</h5>
+                    <a class="d-flex align-items-end mb-2" href="{{ route('tags.show', ['name' => 'PHP']) }}">
+                    <i class="si fab fa-php fa-2x mr-2"></i>PHP</a>
 
-          </div>
+                    <p class="d-flex align-items-end mb-2" style="color: #C71585;">
+                    <i class="si fab fa-docker fa-2x mr-2"></i>docker</p>
 
-          <div class="card col-md-2 m-2">
-            <a href="{{ route('tags.show', ['name' => 'Laravel']) }}">
-              <div class="card-body text-center">
-                <h5 class="card-title">Laravel</h5>
-                <i class="si fab fa-laravel fa-3x pb-3"></i>
-                <h6 class="card-subtitle mb-2">2020</h6>
-              </div>
-            </a>
-          </div>
+                    <a class="d-flex align-items-end mb-2" href="{{ route('tags.show', ['name' => 'Laravel']) }}">
+                    <i class="si fab fa-laravel fa-2x mr-2"></i>Laravel</a>
 
-          <div class="card col-md-2 m-2">
-            <a href="{{ route('tags.show', ['name' => 'Nodejs']) }}">
-              <div class="card-body text-center">
-                <h5 class="card-title">Node.js</h5>
-                <i class="si fab fa-node-js fa-3x pb-3"></i>
-                <h6 class="card-subtitle mb-2">2020</h6>
-              </div>
-            </a>
-          </div>
+                    <a class="d-flex align-items-end mb-2" href="{{ route('tags.show', ['name' => 'Bootstrap']) }}">
+                    <i class="si fab fa-bootstrap fa-2x mr-2"></i>Bootstrap</a>
 
-          <div class="card col-md-2 m-2">
-            <a href="{{ route('tags.show', ['name' => 'Bootstrap']) }}">
-              <div class="card-body text-center">
-                <h5 class="card-title">Bootstrap</h5>
-                <i class="si fab fa-bootstrap fa-3x pb-3"></i>
-                <h6 class="card-subtitle mb-2">2020</h6>
-              </div>
-            </a>
-          </div>
+                    <a class="d-flex align-items-end mb-2" href="{{ route('tags.show', ['name' => 'git']) }}">
+                    <i class="si fab fa-git-alt fa-2x mr-2"></i>git</a>
 
-          <div class="card col-md-2 m-2">
-            <a href="{{ route('tags.show', ['name' => 'SQL']) }}">
-              <div class="card-body text-center">
-                <h5 class="card-title">SQL</h5>
-                <i class="si fas fa-database fa-3x pb-3"></i>
-                <h6 class="card-subtitle mb-2">2020</h6>
-              </div>
-            </a>
-          </div>
+                    <a class="d-flex align-items-end mb-2" href="https://github.com/ayareily/">
+                    <i class="si fab fa-github fa-2x mr-2"></i>GitHub</a>
+                </li>
 
-          <div class="card col-md-2 m-2">
-            <a href="{{ route('tags.show', ['name' => 'docker']) }}">
-              <div class="card-body text-center">
-                <h5 class="card-title">docker</h5>
-                <i class="si fab fa-docker fa-3x pb-3"></i>
-                <h6 class="card-subtitle mb-2">2020</h6>
-              </div>
-            </a>
-          </div>
+                <li class="list-group-item">
+                  <h5 class="list-group-item-heading">2020.4</h5>
+                    <a class="d-flex align-items-end mb-2" href="{{ route('tags.show', ['name' => 'Nodejs']) }}">
+                    <i class="si fab fa-node-js fa-2x mr-2"></i>Node.js</a>
+                </li>
 
-          <div class="card col-md-2 m-2">
-            <a href="{{ route('tags.show', ['name' => 'git']) }}">
-              <div class="card-body text-center">
-                <h5 class="card-title">git</h5>
-                <i class="si fab fa-git-alt fa-3x pb-3"></i>
-                <h6 class="card-subtitle mb-2">2020</h6>
-              </div>
-            </a>
-          </div>
+                <li class="list-group-item">
+                  <h5 class="list-group-item-heading">2020.5</h5>
+                    <a class="d-flex align-items-end mb-2" href="{{ route('tags.show', ['name' => 'SQL']) }}">
+                    <i class="si fas fa-database fa-2x mr-2"></i>SQL</a>
+                </li>
+              </ul>
 
-          <div class="card col-md-2 m-2">
-            <a href="https://github.com/ayareily/">
-              <div class="card-body text-center">
-                <h5 class="card-title">GitHub</h5>
-                <i class="si fab fa-github fa-3x pb-3"></i>
-                <h6 class="card-subtitle mb-2">2020</h6>
-              </div>
-            </a>
           </div>
 
         </div>
@@ -175,7 +133,7 @@
             <div class="card-body text-center">
               <h5 class="card-title">フォント</h5>
                   <i class="si fas fa-font fa-3x pb-3"></i>
-                  <h6 class="card-subtitle mb-2">フリーフォントを眺めています。</h6>
+                  <h6 class="card-subtitle mb-2">MSゴシックが敵。</h6>
             </div>
           </div>
         </div>
